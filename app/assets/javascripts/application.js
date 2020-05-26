3c3934df59bc12bdb7787d85c5c25e13085f9302
rishabhmatta20 <rishabhmatta20@gmail.com>
2020-05-27 01:40:14 +0530

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+// --------------------- Vendor ------------------------ //
+
+//= require animsition/animsition
+//= require animsition/animsition.min
+//= require jquery/jquery-3.2.1.min
+//= require bootstrap/bootstrap
+//= require bootstrap/bootstrap.min
+//= require popper/popper
+//= require popper/popper.min
+//= require tooltip/tooltip
+//= require count_down_time/countdowntime
+//= require date_range_picker/daterangepicker
+//= require date_range_picker/moment
+//= require date_range_picker/moment.min
+//= require perfect_scrollbar/perfect-scrollbar.min
+//= require select2/select2
+//= require select2/select2.min
+
+// --------------------- /Vendor ------------------------ //
